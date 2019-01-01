7a155c2d90ad0bb8ddb126207510b15e23c2ae73
lynnphayu <lynnphayu@icloud.com>
2019-01-01 18:05:10 +0630

    Initial Commit

---
+++ Forex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Forex
+//
+//  Created by Lynn Phay U on 1/1/19.
+//  Copyright © 2019 Lynn Phay U. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
